b481747b5c6cfb93be513a77a2bed3106097f541
Pete Browne <me@petebrowne.com>
2012-11-19 13:43:53 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-import os
 from setuptools import find_packages, setup
 
 from assetfiles import __version__
-
-root = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(root, 'README.md'), 'r') as file:
-    README = file.read()
 
 setup(
     name='django-assetfiles',
     version=__version__,
     description='Drop-in replacement for staticfiles which handles asset processing.',
-    long_description=README,
     author='LocalMed',
+    author_email='pete.browne@localmed.com',
     url='http://github.com/LocalMed/django-assetfiles',
     license='MIT',
     packages=find_packages(),
